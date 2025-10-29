fe101379f4a20a16d7498c111e613b3346df4793
wccalvin <clayton@wccalvin.com>
2025-10-29 17:24:54 -0400

    example of primitive types and variables

---
+++ foundations/01_variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+  int i;  // contains junk value without initializing
+  std::cout << i << std::endl;
+
+  int j = 0;
+  std::cout << j << std::endl;
+
+  char a = 'c';
+  std::cout << a << std::endl;
+
+  bool b = true;
+  std::cout << b << std::endl;
+
+  float f = 1.345f;
+  std::cout << f << std::endl;
+
+  double d = 1.345;
+  std::cout << d << std::endl;
+
+  return 0;
+}
