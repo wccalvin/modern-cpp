90433cd1ee012a03cadbd463fbafb4b6f787c12e
wccalvin <clayton@wccalvin.com>
2025-11-15 14:03:01 -0500

    overview of reference

--- foundations/05_references.cpp
+++ foundations/05_references.cpp
@@ -13,6 +13,16 @@
 
 int main() {
   int x = 7;
-  int &ref = x;  // x is referent
+  int& ref = x;  // x is referent and ref is reference to x
+  std::cout << "Value of x: " << x << std::endl;
+  std::cout << "Value of ref: " << ref
+            << std::endl;  // dereferencing not needed
+  std::cout << "Address of x: " << &x << std::endl;
+  std::cout << "Address of ref: " << &ref << std::endl;
+  // modifying value through reference
+  ref = 20;
+  std::cout << "Modified Value of x by modifying ref: " << x << std::endl;
+  std::cout << "Value of ref after modifying x: " << ref << std::endl;
+
   return 0;
 }
